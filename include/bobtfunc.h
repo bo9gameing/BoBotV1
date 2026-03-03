c29bfed45100da05e3083dca6469a4001f1ca1f7
bo9gameing <bo9gaming55@gmail.com>
2026-03-02 21:52:21 -0600

    added name system pt 0.5

--- include/bobtfunc.h
+++ include/bobtfunc.h
@@ -6,20 +6,20 @@ int test(int a, int b){
 }
 
 
-int isItBoBotTest(char greeter[],char name[]){
+int isItBoBot(char greeter[],char name[]){
      char chatStarter[] = "hay";
      char nameForBo[] = "bobot"; 
 
      if(strcmp(chatStarter,greeter) == 0){
           //later code TODO
      }else{
-          printf("Sysyem fail");
+          printf("Sysyem fail on greeter\n");
           return 1;
      }
      if(strcmp(nameForBo,name) == 0){
           //later code TODO
      }else{
-          printf("Sysyem fail");
+          printf("Sysyem fail on name \n");
           return 1;
      }
      return 0;
--- src/main.c
+++ src/main.c
@@ -1,24 +1,45 @@
-#include "../include/botest.h"
+
+//system fiels
+#include <stdbool.h>
+
+//local hedder fiels
+#include "../include/bobtfunc.h"
 
 
+//definds and non changing vars
+
+
+
+//call functions 
 int startCall();
+int actvateBoBot();
 
+
+
+//program wide vars
+int programEnd = false;
 
 int main(int argc, char const *argv[])
 {
     //vars
     int testCallPorF;
 
-
+    //enter main loop for the code
 
     // comit test
     testCallPorF = startCall();
-    return testCallPorF;
+    if(testCallPorF == 0){
+        printf("all tests pass\n");
+    }else{
+        return testCallPorF;
+    }
+    actvateBoBot();
+    return 0;
 }
 
 
 
-
+//json file fore example
 
 
 int startCall()
@@ -29,11 +50,30 @@ int startCall()
 
 
 
-    int passGreter = isItBoBotTest(greater,name);
+    int passGreter = isItBoBot(greater,name);
     if(passGreter == 0){
         printf("Test Pass\n");
     }else{
         return 1;
     }
     return 0;
+}
+
+
+
+
+/*
+    In this functoin we call to see if the name the user type is the name botbot in that spelling so that it is readable to the code.
+    once this is done it will return a vall of pass or 4 saying that it has pass reconisasion and ready for the next part.
+
+
+*/
+int actvateBoBot(){
+    char name[5];
+    scanf("%s", &name);
+    int passBoBot = isItBoBot("hay",name);
+    if(passBoBot == 0){
+        printf("Hello user\n");
+        return 4;
+    }
 }
